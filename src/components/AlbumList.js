587e7024a3a87e33e1021b052d16f279cb6f34a1
UchuNoKen <xen.o.fite@gmail.com>
2019-05-21 13:56:28 -0400

    use axios for get request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import axios from "axios";
 
 class AlbumList extends Component {
+  componentWillMount() {
+    // console.log("will mount in album list");
+
+    axios
+      .get("https://rallycoding.herokuapp.com/api/music_albums")
+      .then(response => console.log(response));
+  }
+
   render() {
     return (
       <View>
